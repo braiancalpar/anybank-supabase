8251598ab09d94a341377d788eb5b3d99c49ff3c
braiancalpar <braian.santos@calpar.com.br>
2026-09-03 09:14:26 -0300

    corrige importação do AuthProvider e adiciona função de login no contexto de autenticação

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/Header";
 import { Outlet } from "react-router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { AuthProvider } from "./app/context/authContext";
+import { AuthProvider } from "./app/context/AuthContext";
 
 const Container = styled.div`
   display: flex;
