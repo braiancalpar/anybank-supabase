0fe0f7ebac33f74171afc5bc0634171ce0971e2d
braiancalpar <braian.santos@calpar.com.br>
2026-09-03 08:53:15 -0300

    adiciona contexto de autenticação e implementa provedor AuthProvider

---
+++ src/app/context/authContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { supabase } from "../../infra/supabase/config";
+import { Session } from "@supabase/supabase-js";
+
+interface IAuthContext {
+  logout: () => Promise<void>;
+  session: Session | null;
+}
+
+export const AuthContext = createContext<IAuthContext | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      console.log("getSession", session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+      console.log("onAuthStateChange", session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const logout = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return (
+    <AuthContext.Provider value={{ session, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,47 +1,50 @@
-import { Link } from "react-router-dom"
-import { Button } from "../Button"
-import { IconAvatar, IconLogo } from "../Icons"
-import { TransparentButton } from "../TransparentButton"
-import { Container, StyledHeader, List, ListItem } from "./styles"
+import { Link } from "react-router-dom";
+import { Button } from "../Button";
+import { IconAvatar, IconLogo } from "../Icons";
+import { TransparentButton } from "../TransparentButton";
+import { Container, StyledHeader, List, ListItem } from "./styles";
+import { useAuthContext } from "../../app/hooks/useAuthContext";
 
 export const Header = () => {
+  const { logout } = useAuthContext();
 
-    const onAskForLogout = () => {
-        console.log('logout')
-    }
+  const onAskForLogout = () => {
+    console.log("logout");
+    logout();
+  };
 
-    return (<StyledHeader>
-        <Container>
-            <List>
-                <ListItem>
-                    <Link to="/">
-                        <IconLogo />
-                    </Link>
-                </ListItem>
-            </List>
-            <List>
-                <ListItem>
-                    <Button link to="/auth/register">
-                        Abrir conta
-                    </Button>
-                </ListItem>
-                <ListItem>
-                    <Button outline link to="/auth/login">
-                        Login
-                    </Button>
-                </ListItem>
-                <ListItem>
-                    Joana da Silva Oliveira
-                </ListItem>
-                <ListItem>
-                    <IconAvatar />
-                </ListItem>
-                <ListItem>
-                    <TransparentButton onClick={onAskForLogout}>
-                        Logout
-                    </TransparentButton>
-                </ListItem>
-            </List>
-        </Container>
-    </StyledHeader>)
-}
+  return (
+    <StyledHeader>
+      <Container>
+        <List>
+          <ListItem>
+            <Link to="/">
+              <IconLogo />
+            </Link>
+          </ListItem>
+        </List>
+        <List>
+          <ListItem>
+            <Button link to="/auth/register">
+              Abrir conta
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Button outline link to="/auth/login">
+              Login
+            </Button>
+          </ListItem>
+          <ListItem>Joana da Silva Oliveira</ListItem>
+          <ListItem>
+            <IconAvatar />
+          </ListItem>
+          <ListItem>
+            <TransparentButton onClick={onAskForLogout}>
+              Logout
+            </TransparentButton>
+          </ListItem>
+        </List>
+      </Container>
+    </StyledHeader>
+  );
+};
